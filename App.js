bbadc0e1fc03215f8afe255f9d55ade9166f9247
jdiperi88 <jdiperi88@yahoo.com>
2018-08-18 08:06:04 -0400

    added slider

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet, Slider } from 'react-native';
 import AddEntry from './components/AddEntry'
+
 export default class App extends React.Component {
+  state = {
+    value: 0 
+  }
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <AddEntry />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    alignItems: 'stretch',
+    justifyContent: 'center'
+  }
+})
