f79a7d3d4ed2a4329117b221bf43bb6134838e6d
jdiperi88 <jdiperi88@yahoo.com>
2018-08-21 21:31:21 -0400

    added history and metriccard components

--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,10 @@
 import { AsyncStorage } from 'react-native'
-import { CALENDAR_STORAGE_KEY } from './_calandar'
+import { formatCalendarResults, CALENDAR_STORAGE_KEY } from './_calandar'
+
+export function fetchCalendarResults () {
+  return AsyncStorage.getItem(CALENDAR_STORAGE_KEY)
+    .then(formatCalendarResults)
+}
 
 export function submitEntry({entry, key }){
     return AsyncStorage.mergeItem(CALENDAR_STORAGE_KEY,JSON.stringify({
